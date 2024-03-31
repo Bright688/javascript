c4c1b39bb2933144323e83442c49ce048b143857
Bright686 <akaraiwebright0@gmail.com>
2024-03-31 12:39:48 +0000

    javascript exercise#4

---
+++ Exercise/exercise4.js
@@ -0,0 +1,10 @@
+//create an array named colors that include list of colors
+let colors=['red', 'green', 'blue'];
+// add color to the end of the array
+colors.push('black');
+console.log(colors);
+//remove the value red and swap the position of the value green and blue.
+colors.shift();
+colors.unshift('blue');
+colors.unshift('yellow')
+console.log(colors);
